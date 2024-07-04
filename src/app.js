327f1a0571da108037cde09d7601fea14d3a1a0d
Maurcio Heredia <maurih46322945@gmail.com>
2024-07-04 14:48:09 -0300

    Se creo el GET y el POST

--- src/app.js
+++ src/app.js
@@ -13,3 +13,48 @@ app.set("port", process.env.PORT || 4321);
 const students = []
 
 let id = 1;
+
+//CRUD DE ESTUDIANTES
+
+//Lista de todos los estudiantes
+app.get("/students", (req, res) => {
+    res.send(students);
+});
+
+//Devuelve un estudiante
+app.get("/students/:id", (req, res) => {
+    const { id } = req.params;
+    const student = students.find(student => student.id === parseInt(id));
+    if (!student) {
+        return res.status(404).json("Estudiante no encontrado");
+    }
+    res.send(student);
+});
+
+//Crea un estudiante
+app.post("/students", (req, res) => {
+    const {fullname, age, curse} = req.body;
+
+    if (!fullname || !age || !curse) {
+        return res.status(400).json("Faltan datos");
+    }
+
+    const exitestudent = students.find(student => student.fullname === fullname);
+    if (exitestudent) {
+        return res.status(400).json("El estudiante ya existe");
+    }
+
+    const student = {
+        id: id++,
+        fullname,
+        age,
+        curse
+    };
+    students.push(student);
+    res.status(201).send(student);
+});
+
+
+app.listen(app.get("port"), () => {
+    console.log(`SERVIDOR EN EL PUERTO ${app.get("port")}`);
+})
